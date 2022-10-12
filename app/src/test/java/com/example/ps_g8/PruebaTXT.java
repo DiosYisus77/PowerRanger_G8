4d8603666cc04e169f229dae3978bf5495e0feee
DiegoDAI <80209861+DiegoDAI@users.noreply.github.com>
2022-10-12 13:25:46 +0200

    Cargar Fichero(Versión 2)

---
+++ app/src/test/java/com/example/ps_g8/PruebaTXT.java
@@ -0,0 +1,20 @@
+package com.example.ps_g8;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class PruebaTXT {
+    @Test
+    public void probarFichero() throws FileNotFoundException {
+        Database db= new Database();
+        ArrayList<Pelicula> peli = new ArrayList<Pelicula>();
+        db.cargarListaPelicula();
+        peli = db.getPelis();
+
+        assertEquals(peli, db.getPelis());
+    }
+}
