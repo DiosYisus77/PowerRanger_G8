5776e1769817cbb37dc6c0aef5455a4eab42aee0
DiosYisus77 <jesusp.sanchez77@gmail.com>
2022-11-19 14:46:36 +0100

    problema registro solucionado

--- app/src/main/java/com/example/ps_g8/MainActivity5.java
+++ app/src/main/java/com/example/ps_g8/MainActivity5.java
@@ -2,6 +2,7 @@ package com.example.ps_g8;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
@@ -13,14 +14,16 @@ import android.widget.Toast;
 
 public class MainActivity5 extends AppCompatActivity {
 
-    private EditText et_contraseña, et_email;
+    private EditText et_password1, et_password2, et_email;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
 
         et_email = (EditText) findViewById(R.id.id_Email3);
-        et_contraseña =(EditText) findViewById(R.id.id_Password3);
+        et_password1 = (EditText) findViewById(R.id.id_Password1);
+        et_password2 = (EditText) findViewById(R.id.id_Password2);
     }
 
     public void cancelRegister(View view){
@@ -28,41 +31,48 @@ public class MainActivity5 extends AppCompatActivity {
         startActivity(cancel);
     }
 
-    public void Registrarse(View view) {
+    public void Register(View view) {
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "Administracion", null, 1);
         SQLiteDatabase BaseDatos = admin.getWritableDatabase();
 
         String email = et_email.getText().toString();
-        String contraseña = et_contraseña.getText().toString();
+        String password1 = et_password1.getText().toString();
+        String password2 = et_password2.getText().toString();
 
-        if ((!email.isEmpty()) && (!contraseña.isEmpty())) {
-            ContentValues registro = new ContentValues();
-
-            if (!isUsuario(email)) {
-                registro.put("email", email);
-                registro.put("contraseña", contraseña);
-                BaseDatos.insert("listaPelicula", null, registro);
-                BaseDatos.close();
-
-                et_email.setText("");
-                et_contraseña.setText("");
-
-                Toast.makeText(getApplicationContext(), "Registro completado", Toast.LENGTH_SHORT).show();
-
-                Intent confirmar = new Intent(this, MainActivity.class);
-                startActivity(confirmar);
-                //}
-
-            } else {
-                Toast.makeText(getApplicationContext(), "Debes rellenar todos los campos", Toast.LENGTH_SHORT).show();
+        if ((!email.isEmpty()) && (!password1.isEmpty()) && (!password2.isEmpty())) {
+            if (!(password1.equals(password2))){
+                Toast.makeText(getApplicationContext(), "ERROR: Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
             }
+            else {
+                ContentValues registro = new ContentValues();
+                if (isUsuario(email)==false) {
+                    registro.put("email", email);
+                    registro.put("contraseña", password1);
+                    BaseDatos.insert("usuario", null, registro);
+                    BaseDatos.close();
+
+                    et_email.setText("");
+                    et_password1.setText("");
+                    et_password2.setText("");
+
+                    Toast.makeText(getApplicationContext(), "Registro completado", Toast.LENGTH_SHORT).show();
+
+                    Intent confirmar = new Intent(this, MainActivity.class);
+                    startActivity(confirmar);
+                } else {
+                    Toast.makeText(getApplicationContext(), "ERROR: Este email ya está registrado", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        } else {
+            Toast.makeText(getApplicationContext(), "ERROR: Por favor, rellena todos los campos", Toast.LENGTH_SHORT).show();
         }
     }
 
     public boolean isUsuario(String email) {
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "Administracion", null, 1);
         SQLiteDatabase BaseDatos = admin.getWritableDatabase();
-        Cursor fila = BaseDatos.rawQuery("select contraseña from lista where email =?" + email, null);
+        Cursor fila = BaseDatos.rawQuery("select email from usuario where email =" + email, null);
         if (fila.moveToFirst()) {
             BaseDatos.close();
             return true;
